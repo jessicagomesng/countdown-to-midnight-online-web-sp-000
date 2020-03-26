922d9404956a067823216bde51d2c2d3112989cf
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-26 12:12:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,16 @@ def countdown(x)
   end
   if x == 0
     puts "HAPPY NEW YEAR!"
-  end 
+  end
 end
 
-countdown(10)
+def countdown_with_sleep(y)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1.second
+  end
+  if x == 0
+    puts "HAPPY NEW YEAR!"
+  end
+end
